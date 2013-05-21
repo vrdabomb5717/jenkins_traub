9d11e4c3dcb168a6b87ad41a880e9cd4c839e9fc
Varun Ravishankar <vrdabomb@gmail.com>
2013-05-20 23:20:56 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,73 @@
+Title: Finding Roots of a Polynomial Using Jenkins-Traub
+Author: Varun Ravishankar
+Email: vr2263@columbia.edu
+Date: May 4th, 2013
+
+# Finding Roots of a Polynomial Using Jenkins-Traub
+## by Varun Ravishankar
+
+This program finds the roots of a polynomial using the Jenkins-Traub algorithm.
+
+
+# Requirements
+
+* Python 2.7
+* Numpy
+
+Both of these are available on the Columbia University CLIC lab machines.
+
+# Usage
+
+`$ python jenkins_traub.py -c [coefficients] -e [epsilon] -m [max_iterations]`
+
+where
+
+* `c` is a white-space delimited list of the coefficients. Coefficients can be complex.
+* `e` is `EPSILON`, the epsilon value used to determine algorithm stopping conditions.
+* `m` is `MAX_ITERATIONS`, the max number of iterations for the algorithm to run.
+
+The coefficients can be complex. To specify a complex coefficient, use j instead of i. For example, to specify the number *1-i*, use `1-1j`. To specify *i*, use `1j`. If the complex part of a coefficient is 1, you must specify it. You may not use spaces to specify complex coefficients; `1 - 1j` will not work.
+
+The max iterations by default is set to 1000, and epsilon by default is set to 1.0e-10.
+
+
+# Examples
+
+```
+$ python jenkins_traub.py -c 1 -9.01 27.08 -41.19 32.22 -10.1
+Done with Stage 1 after 5 iterations.
+Done with Stage 2 after 2 iterations.
+Done with Stage 3 after 7 iterations.
+Done with Stage 1 after 5 iterations.
+Done with Stage 2 after 2 iterations.
+Done with Stage 3 after 6 iterations.
+Done with Stage 1 after 5 iterations.
+Done with Stage 2 after 2 iterations.
+Done with Stage 3 after 6 iterations.
+Done with Stage 1 after 5 iterations.
+Done with Stage 2 after 2 iterations.
+Done with Stage 3 after 4 iterations.
+Roots: [(1.0000000020052087-4.6143098772373481e-10j), (1.0099999979899685+4.6254009123783083e-10j), (1.0000000000000868+0.99999999999002043j), (1.0000000000044402-0.99999999999106171j), (5.0000000000002958-6.7834626804597065e-14j)]
+
+$ python -O jenkins_traub.py -c 1 -4 4
+Done with Stage 1 after 5 iterations.
+Done with Stage 2 after 2 iterations.
+Done with Stage 3 after 1 iterations.
+Roots: [(1.9999999999999996+5.5511151231257827e-16j), (2.0000000000000004-5.5511151231257827e-16j)]
+
+$ python -O jenkins_traub.py -c 1+1j 10 0 2-5j 8 545
+Done with Stage 1 after 5 iterations.
+Done with Stage 2 after 4 iterations.
+Done with Stage 3 after 3 iterations.
+Done with Stage 1 after 5 iterations.
+Done with Stage 2 after 4 iterations.
+Done with Stage 3 after 3 iterations.
+Done with Stage 1 after 5 iterations.
+Done with Stage 2 after 4 iterations.
+Done with Stage 3 after 4 iterations.
+Done with Stage 1 after 5 iterations.
+Done with Stage 2 after 8 iterations.
+Done with Stage 3 after 4 iterations.
+Roots: [(-2.1412696593208285+2.1444016657547995j), (-1.7159723186584845-2.0530205561017132j), (1.7466808485599237-1.7876397485309532j), (2.0691454602009416+1.763758058933768j), (-4.9585843307815534+4.9325005799440991j)]
+
+```
---
+++ jenkins_traub.py
@@ -0,0 +1,297 @@
+#!/usr/bin/env python
+
+"""Find the roots of a polynomial using the Jenkins-Traub algorithm."""
+
+
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import re
+from functools import partial
+
+import numpy as np
+
+
+class ConvergenceError(Exception):
+    pass
+
+
+def evaluate(coefficients):
+    """Evaluate a polynomial with given coefficients in highest to lowest order."""
+    return lambda s: synthetic_division(coefficients, s)[1]
+
+
+def newton(x_0, function, first_derivative, epsilon, max_iterations):
+    """Find the root of a polynomial using Newton-Raphson iteration."""
+    x_i = x_0
+    num_iterations = 0
+    x_i_next = x_i - (function(x_i) / first_derivative(x_i))
+    num_iterations += 1
+
+    while abs(x_i_next - x_i) > epsilon and num_iterations < max_iterations:
+        x_i = x_i_next
+        x_i_next = x_i - (function(x_i) / first_derivative(x_i))
+        num_iterations += 1
+
+    return x_i
+
+
+def synthetic_division(coefficients, s):
+    """Perform synthetic division and evaluate a polynomial at s."""
+    deflated = np.empty(len(coefficients) - 1, dtype=np.complex128)
+
+    deflated[0] = coefficients[0]
+
+    for i, a_i in enumerate(coefficients[1:-1], start=1):
+        deflated[i] = a_i + deflated[i - 1] * s
+
+    evaluation = coefficients[-1] + deflated[-1] * s
+
+    return deflated, evaluation
+
+
+def stage1(a, H_lambda, epsilon, max_iterations):
+    """Perform Stage 1, the no-shift process of Jenkins-Traub.
+
+    Returns the deflated polynomial, and the number of iterations the stage took.
+
+    """
+    for i in xrange(max_iterations):
+        H_bar_lambda, _ = stage2(a, H_lambda, 0, epsilon, 1)
+
+    return H_lambda, max_iterations
+
+
+def stage2(a, H_lambda, s, epsilon, max_iterations):
+    """Perform Stage 2, the fixed shift process of Jenkins-Traub.
+
+    Returns the deflated polynomial, and the number of iterations the stage took.
+
+    """
+    t = t_prev = t_prev_prev = float('inf')
+    num_iterations = 0
+
+    H_bar_lambda = H_lambda / H_lambda[0]
+
+    while True:
+        p, p_at_s = synthetic_division(a, s)
+        h_bar, h_bar_at_s = synthetic_division(H_bar_lambda, s)
+
+        # If we found a root, short circuit the other logic.
+        # Used for when we found a root exactly, and we'd end up dividing
+        # by 0 when finding s_lambda
+        if np.absolute(p_at_s) < epsilon:
+            return H_bar_lambda, num_iterations
+
+        if np.absolute(h_bar_at_s) < epsilon:
+            h_bar_at_s += epsilon / 100
+
+        h_bar = np.insert(h_bar, 0, 0)  # watch for polynomial length differences
+        H_bar_lambda_next = p - (p_at_s / h_bar_at_s) * h_bar
+
+        t_prev, t_prev_prev = t, t_prev
+        t = s - p_at_s / h_bar_at_s
+
+        H_bar_lambda = H_bar_lambda_next
+        num_iterations += 1
+
+        condition1 = np.absolute(t_prev - t_prev_prev) <= 0.5 * np.absolute(t_prev_prev)
+        condition2 = np.absolute(t - t_prev) <= 0.5 * np.absolute(t_prev)
+        condition3 = num_iterations > max_iterations
+
+        if (condition1 and condition2) or condition3:
+            break
+
+    return H_bar_lambda, num_iterations
+
+
+def stage3(a, H_L, s, epsilon, max_iterations):
+    """Perform Stage 3, the variable-shift recurrence of Jenkins-Traub.
+
+    Returns the deflated polynomial, the root, and the number of
+    iterations the stage took.
+
+    """
+    polynomial = evaluate(a)
+    H_bar_coefficients = H_L / H_L[0]
+    H_bar = evaluate(H_bar_coefficients)
+    s_L = s - polynomial(s) / H_bar(s)
+
+    H_bar_lambda = H_bar_coefficients.copy()
+    s_lambda = s_L
+    s_lambda_prev = complex('inf')
+
+    num_iterations = 0
+
+    while np.absolute(s_lambda - s_lambda_prev) > epsilon and num_iterations < max_iterations:
+        p, p_at_s_lambda = synthetic_division(a, s_lambda)
+        h_bar, h_bar_at_s_lambda = synthetic_division(H_bar_lambda, s_lambda)
+        h_bar = np.insert(h_bar, 0, 0)  # watch for polynomial length differences
+
+        # If we found a root, short circuit the other logic.
+        # Used for when we found a root exactly, and we'd end up dividing
+        # by 0 when finding s_lambda
+        if np.absolute(p_at_s_lambda) < epsilon:
+            return H_bar_lambda, s_lambda, num_iterations
+
+        H_bar_lambda_next = p - (p_at_s_lambda / h_bar_at_s_lambda) * h_bar
+        H_bar_next = evaluate(H_bar_lambda_next)
+
+        num_iterations += 1
+
+        s_lambda, s_lambda_prev = (s_lambda - p_at_s_lambda / H_bar_next(s_lambda)), s_lambda
+        H_bar_lambda = H_bar_lambda_next
+
+    if num_iterations >= max_iterations:
+        print('Stage 3 could not converge after {} iterations'.format(num_iterations))
+        raise ConvergenceError(num_iterations)
+
+    return H_bar_lambda, s_lambda, num_iterations
+
+
+def jenkins_traub_inner(coefficients, epsilon, max_iterations, do_stage_one=True):
+    """Find the smallest root of a polynomial using the Jenkins-Traub algorithm.
+
+    Returns the deflated polynomial and the (approximately) smallest root.
+
+    """
+    # Find zero root, if any
+    if len(coefficients) > 0 and coefficients[-1] == 0:
+        s_lambda = 0
+        return coefficients, s_lambda
+
+    if len(coefficients) < 2:
+        return
+
+    a = np.array(coefficients)
+
+    powers = np.array(xrange(len(coefficients) - 1, -1, -1))
+
+    # Scale coefficients so leading coefficient is 1
+    if a[0] != 1:
+        a /= a[0]
+
+    if len(a) == 2:
+        s_lambda = -a[-1] / a[0]
+        return None, s_lambda
+
+    # H^0 is the derivative of the polynomial
+    H_0 = powers * a
+    H_0 = H_0[:-1]
+
+    H_lambda = H_0.copy()
+    s_lambda = None
+
+    # At this point, you could perform Stage 1 to accentuate small zeros.
+    # We choose to do this and then move on to Stage 2.
+    if do_stage_one:
+        H_lambda, num_iterations1 = stage1(a, H_0, epsilon, 5)
+        print("Done with Stage 1 after {} iterations.".format(num_iterations1))
+
+    # Get s using a modified Cauchy polynomial and Newton's iteration.
+    # Modified polynomial has coefficients that are the moduli of the
+    # original polynomial, but the last root is -1.
+    modified_coefficients = np.absolute(a)
+    modified_coefficients[-1] *= -1
+    modified_derivative = powers * modified_coefficients
+    modified_derivative = modified_derivative[:-1]
+
+    mod_function = evaluate(modified_coefficients)
+    mod_derivative = evaluate(modified_derivative)
+
+    # Only get beta up to two decimal places.
+    x_0 = 1
+    beta = newton(x_0, mod_function, mod_derivative, 0.01, 500)
+
+    while True:
+        try:
+            phi_random = complex(np.random.random() * 2 * np.pi)
+            s_lambda = np.exp(phi_random * (0+1j)) * beta
+            # s_lambda = 10000+10000j  # use for testing bad convergence
+            H_lambda, num_iterations2 = stage2(a, H_lambda, s_lambda, epsilon, 100)
+            print("Done with Stage 2 after {} iterations.".format(num_iterations2))
+
+            H_lambda, s_lambda, num_iterations3 = stage3(a, H_lambda, s_lambda, epsilon, max_iterations)
+            print("Done with Stage 3 after {} iterations.".format(num_iterations3))
+            return H_lambda, s_lambda
+        except ConvergenceError:
+            max_iterations *= 2
+            continue
+
+    return H_lambda, s_lambda
+
+
+def jenkins_traub(coefficients, epsilon, max_iterations):
+    """Find the roots of a polynomial using the Jenkins-Traub algorithm.
+
+    Wrapper method for the Jenkins-Traub algorithm. Loops through the
+    coefficients, finds a root, divides the coefficients by
+    that smallest root to find a new set of coefficients, and then starts over.
+
+    Returns a list of the roots.
+
+    """
+    # Remove leading coefficients equal to 0
+    while len(coefficients) > 0 and coefficients[0] == 0:
+        coefficients.pop(0)
+
+    if len(coefficients) == 0:
+        print("Fed the zero polynomial. Roots are all complex numbers.")
+        return
+    elif len(coefficients) == 1:
+        print("Fed a constant. There are no roots.")
+        return
+
+    for x in xrange(len(coefficients) - 1):
+        if len(coefficients) > 1:
+            _, s = jenkins_traub_inner(coefficients, epsilon, max_iterations)
+            coefficients, _ = synthetic_division(coefficients, s)
+            yield s
+
+
+def parse_args():
+    def positive(data, datatype=float):
+        """Check if a string converts to a positive number."""
+        msg = "{} is not a positive number.".format(data)
+        try:
+            value = datatype(data)
+        except ValueError:
+            raise argparse.ArgumentTypeError(msg)
+        if value < 0:
+            raise argparse.ArgumentTypeError(msg)
+        return value
+
+    positive_float = partial(positive, datatype=float)
+    positive_int = partial(positive, datatype=int)
+
+    parser = argparse.ArgumentParser(description="""Find the roots of a polynomial
+                                     using the Jenkins-Traub algorithm.""")
+    parser._negative_number_matcher = re.compile(r'^-.+$')
+    parser.add_argument("-c", type=complex, dest='coefficients', nargs='+',
+                        default=[1, -9.01, 27.08, -41.19, 32.22, -10.1],
+                        # default=[1, -4, 4],
+                        # default=[1, 0, 0, 0, -16],
+                        help="The coefficients of the polynomial")
+    parser.add_argument("-e", type=positive_float, dest='epsilon', default=1.0e-10,
+                        help="Epsilon value used to determine algorithm stopping conditions.")
+    parser.add_argument("-m", type=positive_int, dest='max_iterations', default=1000,
+                        help="The max number of iterations for the algorithm to run.")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    coefficients = args.coefficients
+    epsilon = args.epsilon
+    max_iterations = args.max_iterations
+
+    solution = jenkins_traub(coefficients, epsilon, max_iterations)
+    roots = [x for x in solution]
+
+    print("Roots: {}".format(roots))
+
+
+if __name__ == '__main__':
+    main()
